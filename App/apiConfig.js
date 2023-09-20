7300770d686db89f301e2bd755f0d0df9dd96c2d
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2023-09-20 13:57:14 +0100

    updating the webApi

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
     b2cScopes: ["https://srj20230818.onmicrosoft.com/tasks-api/tasks.read"],
-    webApi: "http://localhost:5000/hello"
+    webApi: "https://srjames-617069.azurewebsites.net//hello"
   };
