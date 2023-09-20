54fe503cfd7730fc3adf2a1ec49a182e7414045f
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2023-09-20 13:48:55 +0100

    updating redirectUri

--- App/authConfig.js
+++ App/authConfig.js
@@ -11,7 +11,7 @@ const msalConfig = {
       clientId: "1318e947-74e4-480e-8eea-85b74ceb0f62", // This is the ONLY mandatory field; everything else is optional.
       authority: b2cPolicies.authorities.signUpSignIn.authority, // Choose sign-up/sign-in user-flow as your default.
       knownAuthorities: [b2cPolicies.authorityDomain], // You must identify your tenant's domain as a known authority.
-      redirectUri: "https://ashy-tree-0985f7c03.3.azurestaticapps.net/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
+      redirectUri: "https://srjames-737061.azurewebsites.net/", // You must register this URI on Azure Portal/App Registration. Defaults to "window.location.href".
     },
     cache: {
       cacheLocation: "sessionStorage", // Configures cache location. "sessionStorage" is more secure, but "localStorage" gives you SSO between tabs.
