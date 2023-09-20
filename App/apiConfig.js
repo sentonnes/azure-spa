b14f724cea1f2ada51713692380195cbc2e9564d
srjames-ukho <126559680+srjames-ukho@users.noreply.github.com>
2023-09-20 11:58:11 +0100

    updating settings

--- App/apiConfig.js
+++ App/apiConfig.js
@@ -1,5 +1,5 @@
 // The current application coordinates were pre-registered in a B2C tenant.
 const apiConfig = {
-    b2cScopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
-    webApi: "https://fabrikamb2chello.azurewebsites.net/hello"
+    b2cScopes: ["https://srj20230818.onmicrosoft.com/tasks-api/tasks.read"],
+    webApi: "http://localhost:5000/hello"
   };
--- App/policies.js
+++ App/policies.js
@@ -5,16 +5,20 @@
  */
 const b2cPolicies = {
     names: {
-        signUpSignIn: "B2C_1_susi_reset_v2",
-        editProfile: "B2C_1_edit_profile_v2"
+        signUpSignIn: "b2c_1_susi",
+        forgotPassword: "b2c_1_reset_password",
+        editProfile: "b2c_1_edit_profile"
     },
     authorities: {
         signUpSignIn: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_susi_reset_v2",
+            authority: "https://srj20230818.b2clogin.com/srj20230818.onmicrosoft.com/b2c_1_susi",
+        },
+        forgotPassword: {
+            authority: "https://srj20230818.b2clogin.com/srj20230818.onmicrosoft.com/b2c_1_reset_password",
         },
         editProfile: {
-            authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/B2C_1_edit_profile_v2"
+            authority: "https://srj20230818.b2clogin.com/srj20230818.onmicrosoft.com/b2c_1_edit_profile"
         }
     },
-    authorityDomain: "fabrikamb2c.b2clogin.com"
+    authorityDomain: "srj20230818.b2clogin.com"
 }
